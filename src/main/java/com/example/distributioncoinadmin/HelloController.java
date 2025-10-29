4699cae23d076bc531798d4b1764790066da425c
mostdream <mostdream@naver.com>
2025-10-29 13:50:17 +0900

    auto build & deploy test 2

--- src/main/java/com/example/distributioncoinadmin/HelloController.java
+++ src/main/java/com/example/distributioncoinadmin/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Spring Boot is running. ^^";
+        return "Spring Boot is running!!!!!!!";
     }
 }
