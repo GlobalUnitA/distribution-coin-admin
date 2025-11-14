a0fa69b8cfa375003581f8da7635098b3e9431c4
mostdream <mostdream@naver.com>
2025-11-14 16:06:04 +0900

    auto build & deploy test 8

--- src/main/java/com/example/distributioncoinadmin/HelloController.java
+++ src/main/java/com/example/distributioncoinadmin/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Spring Boot is running.";
+        return "Spring Boot is running.!!!";
     }
 }
