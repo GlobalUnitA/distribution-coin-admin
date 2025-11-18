9ee00d63900d528f428ec57325a331de7058966f
mostdream <mostdream@naver.com>
2025-11-18 14:20:50 +0900

    테스트용 데이터 삽입. : 오기입된 칼럼명 수정.

--- src/main/resources/db/migration/V8__insert_default_user.sql
+++ src/main/resources/db/migration/V8__insert_default_user.sql
@@ -3,8 +3,8 @@ INSERT INTO users (
     password_hash,
     name,
     enabled,
-    create_at,
-    update_at
+    created_at,
+    updated_at
 ) VALUES (
  'admin',
  '$2a$10$xF6Ukl1LMXKlUf0wqWvz6.JFGjbYRKwpJnQG5O4T3Am7VfXYHLmzi',
