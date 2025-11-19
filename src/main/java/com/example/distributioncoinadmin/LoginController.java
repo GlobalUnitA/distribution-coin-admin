96b352a3485c604750d57677522cc2d3044b8da2
mostdream <mostdream@naver.com>
2025-11-19 10:49:28 +0900

    로그인 여부 노출 및 로그아웃 버튼 추가.

--- src/main/java/com/example/distributioncoinadmin/LoginController.java
+++ src/main/java/com/example/distributioncoinadmin/LoginController.java
@@ -2,6 +2,8 @@ package com.example.distributioncoinadmin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
 
 @Controller
 public class LoginController {
@@ -12,7 +14,9 @@ public class LoginController {
     }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Authentication authentication, Model model) {
+        String username = authentication.getName();
+        model.addAttribute("username", username);
         return "home";   // templates/home.html
     }
 }
