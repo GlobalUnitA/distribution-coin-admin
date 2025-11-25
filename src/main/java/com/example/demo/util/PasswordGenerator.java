b5cd2af86cab0efb332b41e8d266af5cf85669d6
mostdream <mostdream@naver.com>
2025-11-25 18:47:46 +0900

    1.출금 템플릿 : 왼쪽 사이드바 스타일 추가 및 네트워크 선택 영역 정리. 2.입금 템플릿 : 코인 및 네트워크 드롭다운 영역 정리. 3.패스워드 해시값 대조 테스트 파일 : 관리자2 유저의 것으로 업데이트.

--- src/main/java/com/example/demo/util/PasswordGenerator.java
+++ src/main/java/com/example/demo/util/PasswordGenerator.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordGenerator{
     public static void main(String[] args){
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("*19s4gmfo43md2l*"));
+//        System.out.println(encoder.encode("*19s4gmfo43md2l*"));
+        System.out.println(encoder.encode("*f3kd4la7la8eo*")); //$2a$10$f95hdMCt7C0xwrvDwguL9e8ZsPrk78p8kbyLFslrJ6eK0AbxIaLEG
     }
 }
